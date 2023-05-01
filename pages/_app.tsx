d955d6fba0c662b14b427a6bd9a1f29519912e71
Javad Karimnia <mjavad.k17@gmail.com>
2023-05-01 13:23:57 -0700

    All the basic next.js files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+import Authenticator from '@/app/context/Authenticator';
+import Layouts from '@/app/layouts/Layouts'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Authenticator>
+      <Layouts>
+        <Component {...pageProps} />
+        <ToastContainer />
+      </Layouts>
+    </Authenticator>
+  )
 }
